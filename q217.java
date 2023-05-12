c95166eea1fed6e46001deadf2164bfa56f76030
acanna2013 <ac.anna2013@gmail.com>
2023-05-12 14:31:52 -0400

    added error checking

--- q217.java
+++ q217.java
@@ -3,12 +3,19 @@ import java.util.HashMap;
 public class q217 {
 	HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>();
 	public boolean containsDuplicate(int[] nums) {
-		for (int val : nums) {
-			dict.put(val, dict.get(val)++);
-			if (dict.get(val) != 1) {
-				return false;
-			}
+		if (dict.get(n) == null) {
+			dict.put(n, 1);
+		} else {
+			int val = dict.get(n) + 1;
+			dict.put(n, val);
 		}
-		return true;
+		if (dict.get(n) > 1) {
+			return true;
+		}
 	}
+	return false;  
 }
+
+
+
+
